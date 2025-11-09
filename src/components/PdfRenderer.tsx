4e762b1ced2767b3051737fb5ad4216ce21d6ce0
Yudhajit <yudhajitchowdhury17@gmail.com>
2025-11-09 15:05:42 +0530

    Adding Page Controls

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -1,8 +1,15 @@
 "use client";
 
+import { ChevronDown, ChevronUp, Loader2 } from "lucide-react";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/Page/AnnotationLayer.css";
 import "react-pdf/dist/Page/TextLayer.css";
+import { toast } from "sonner";
+
+import { useResizeDetector } from "react-resize-detector";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+import { useState } from "react";
 
 interface PdfRendererProps {
   url: string;
@@ -11,16 +18,61 @@ interface PdfRendererProps {
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 const PdfRenderer = ({ url }: PdfRendererProps) => {
+  const { width, ref } = useResizeDetector();
+
+  const [numPages, setNumPages] = useState(0);
+  const [currPage, setCurrPage] = useState(1);
+
   return (
     <div className="w-full bg-white rounded-md shadow flex flex-col items-center">
       <div className="h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2">
-        <div className="flex items-center gap-1.5"></div>
+        <div className="flex items-center gap-1.5">
+          <Button
+            disabled={currPage <= 1}
+            onClick={() => {
+              setCurrPage((prev) => (prev - 1 > 0 ? prev - 1 : prev));
+            }}
+            variant="ghost"
+            aria-label="previous page"
+          >
+            <ChevronDown className="h-4 w-4" />
+          </Button>
+          <div className="flex items-center gap-1.5">
+            <Input value={currPage} className="w-12 h-8" />
+            <p className="text-zinc-700 text-sm space-x-1">
+              <span>/</span>
+              <span>{numPages}</span>
+            </p>
+          </div>
+          <Button
+            onClick={() => {
+              setCurrPage((prev) => (prev + 1 > numPages ? prev : prev + 1));
+            }}
+            variant="ghost"
+            aria-label="next page"
+          >
+            <ChevronUp className="h-4 w-4" />
+          </Button>
+        </div>
       </div>
       <div className="flex-1 w-full max-h-screen">
-        <div></div>
-        <Document file={url} className="max-h-full">
-          <Page pageNumber={1} />
-        </Document>
+        <div ref={ref}>
+          <Document
+            loading={
+              <div className="flex justify-center">
+                <Loader2 className="my-24 h-6 w-6 animate-spin" />
+              </div>
+            }
+            onLoadError={() => {
+              return toast.error("Please try again later");
+            }}
+            onLoadSuccess={({ numPages }) => setNumPages(numPages)}
+            file={url}
+            className="max-h-full"
+          >
+            <Page width={width ? width : 1} pageNumber={currPage} />
+          </Document>
+        </div>
       </div>
     </div>
   );
